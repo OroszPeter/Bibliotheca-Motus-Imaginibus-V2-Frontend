0f7dd0327a33862053acfb9f596f991aa2062393
OroszPeter <petiorosz4@gmail.com>
2024-12-17 09:34:33 +0100

    Film/Sorozat hozzáadás: Felépítés

--- src/routes/add-movie/+page.svelte
+++ src/routes/add-movie/+page.svelte
@@ -26,10 +26,7 @@
             <td rowspan="2">
               <!-- Kép -->
               <div class="picture">
-                <img
-                  src='http://placehold.co/400x600'
-                  class="img-fluid"
-                />
+                <input type="file" placeholder="Kép">
               </div>
             </td>
             <td>
@@ -37,12 +34,17 @@
               <div class="content">
                 <h2>
                   <input type="text" placeholder="Cím">
-                  <button class="btn btn-primary btn-sm ms-2">+</button>
+                  <!-- <button class="btn btn-primary btn-sm ms-2">+</button> -->
                 </h2>
-                <p><strong>Műfaj</strong></p>
-                <small>5.0 &#9733;</small>
+                <p><strong><input type="text" placeholder="Műfaj"></strong></p>
+                <p>Sorozat?</p><input type="checkbox" placeholder="Sorozat?"><br>
+                <input type="number" placeholder="Évadok"><br>
+                <input type="number" placeholder="Epizódok"><br>
+                <input type="date" placeholder="Megjelenési idő"><br>
+                <input type="number" placeholder="Hossz percben">
+                <!-- <small>5.0 &#9733;</small> -->
                 <p>
-                 Leírás
+                  <textarea placeholder="Leírás" rows="10" cols="50"></textarea>
                 </p>
               </div>
             </td>
@@ -59,19 +61,24 @@
                   <tbody>
                     <tr>
                       <td>Rendező</td>
-                      <td>rendező</td>
+                      <td><input type="text" placeholder="Rendező"></td>
                     </tr>
                     <!-- Színészek itt adhatóak hozzá az API válasza alapján -->
                     <tr>
                       <td>Színész 1</td>
-                      <td>Arnold Schwarzenegger</td>
+                      <td><input type="text" placeholder="Színész"></td>
                     </tr>
                     <tr>
                       <td>Színész 2</td>
-                      <td>Linda Hamilton</td>
+                      <td><input type="text" placeholder="Színész"></td>
+                    </tr>
+                    <tr>
+                      <td>Színész 3</td>
+                      <td><input type="text" placeholder="Színész"></td>
                     </tr>
                   </tbody>
                 </table>
+                <button class="btn btn-success">Hozzáad</button>
               </div>
             </td>
           </tr>
@@ -79,20 +86,7 @@
             <td colspan="2">
               <!-- Komment szakasz -->
               <div class="addcomment mt-4">
-                <div class="star-rating mb-3" on:mouseleave={resetRating}>
-                  <!-- 1. csillag -->
-                  <label for="star1" class="star" on:mouseover={() => hoverRating(1)} on:click={() => setRating(1)}>{rating >= 1 ? '★' : '☆'}</label>
-                  <!-- 2. csillag -->
-                  <label for="star2" class="star" on:mouseover={() => hoverRating(2)} on:click={() => setRating(2)}>{rating >= 2 ? '★' : '☆'}</label>
-                  <!-- 3. csillag -->
-                  <label for="star3" class="star" on:mouseover={() => hoverRating(3)} on:click={() => setRating(3)}>{rating >= 3 ? '★' : '☆'}</label>
-                  <!-- 4. csillag -->
-                  <label for="star4" class="star" on:mouseover={() => hoverRating(4)} on:click={() => setRating(4)}>{rating >= 4 ? '★' : '☆'}</label>
-                  <!-- 5. csillag -->
-                  <label for="star5" class="star" on:mouseover={() => hoverRating(5)} on:click={() => setRating(5)}>{rating >= 5 ? '★' : '☆'}</label>
-                </div>
-                <textarea name="komment" cols="100" rows="4"></textarea>
-                <button class="btn btn-success">Küldés</button>
+                
               </div>
             </td>
           </tr>
@@ -103,61 +97,75 @@
   
   <style>
     .container {
-      margin-left: 300px;
-      padding: 20px;
-      color: white;
-      margin-top: 25px;
-    }
-    textarea {
-      border-radius: 10px;
-    }
-  
-    table {
-      width: 100%;
-      table-layout: fixed;
-    }
-  
-    .picture,
-    .content,
-    .people {
-      width: 100%;
-      padding: 10px;
-    }
-  
-    .star {
-      font-size: 2rem;
-      color: #ccc;
-      cursor: pointer;
-      transition: color 0.3s ease;
-    }
-  
-    .star.selected,
-    .star:hover,
-    .star:active {
-      color: gold;
-    }
-  
-    .star-rating label:hover,
-    .star-rating label.selected {
-      color: gold;
-    }
-  
-    .table th,
-    .table td {
-      vertical-align: middle;
-    }
-  
-    .star-rating label {
-      margin: 0 5px;
-    }
-  
-    .addcomment {
-      max-width: 100%;
-    }
-  
-    .star-rating {
-      display: flex;
-      justify-content: flex-start;
-    }
+  margin-left: 300px;
+  padding: 20px;
+  color: white;
+  margin-top: 25px;
+}
+
+textarea {
+  border-radius: 10px;
+}
+
+table {
+  width: 100%;
+  table-layout: fixed;
+}
+
+.picture {
+  border: 1px solid black;
+  width: 400px;
+  height: 700px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.picture input {
+  text-align: center;
+}
+
+.content,
+.people {
+  width: 100%;
+  padding: 10px;
+  vertical-align: top;
+}
+
+.star {
+  font-size: 2rem;
+  color: #ccc;
+  cursor: pointer;
+  transition: color 0.3s ease;
+}
+
+.star.selected,
+.star:hover,
+.star:active {
+  color: gold;
+}
+
+.star-rating label:hover,
+.star-rating label.selected {
+  color: gold;
+}
+
+.table th,
+.table td {
+  vertical-align: middle;
+}
+
+.star-rating label {
+  margin: 0 5px;
+}
+
+.addcomment {
+  max-width: 100%;
+}
+
+.star-rating {
+  display: flex;
+  justify-content: flex-start;
+}
   </style>
   
