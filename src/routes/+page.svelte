5a6c82070235b531462f03cc5b12357666a7ec72
OroszPeter <petiorosz4@gmail.com>
2025-01-07 09:16:05 +0100

    Adott tartalom - Emberek táblázat átrendezése

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
   let ratings = [];
   let isLoading = true;
 
-  // API hívás a filmek és értékelések lekérésére
+  // API hívás a filmek és értékelések lekéréséree
   onMount(async () => {
   try {
     // Filmek és értékelések betöltése
