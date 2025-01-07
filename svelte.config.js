750617ce6ff04276664a4dfc340c8d042bbc51b0
OroszPeter <petiorosz4@gmail.com>
2025-01-07 09:32:08 +0100

    Netlify deployhoz

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,17 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import('@sveltejs/kit').Config} */
+
+/** @type {import('@sveltejs/kit').Config}*/
 const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+    preprocess : vitePreprocess(),
+    kit: {
+        adapter: adapter({
+            fallback: '404.html'
+        }),
+        // Ellenőrizd, hogy van-e bármi extra útvonal konfiguráció, ami akadályozhatja
+        // a dinamikus útvonalak megfelelő működését
+    }
 };
 
-export default config;
+export default config
